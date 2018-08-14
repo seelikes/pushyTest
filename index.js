e52d6ddc04b64cd92f395cd5a23d2343afefc4db
liutiantian <seelikes@163.com>
2018-08-14 16:31:44 +0800

    first version upload

---
+++ index.js
@@ -0,0 +1,8 @@
+/** @format */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import MyProject from './MyProject';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => MyProject);
